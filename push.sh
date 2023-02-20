2736e2dd96239632ba62d6ecbb00ce1bb4757e4c
Frederik Hørgreen <frederik@hoergreen.dk>
2023-02-20 21:13:40 +0100

    main: fix push since we do not sort by version

--- push.sh
+++ push.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -e
 
-CURRENT_VERSION=$(curl -s https://registry.hrgn.dk/v2/planning-poker-api/tags/list | jq '.tags | sort | reverse | .[0]' --raw-output)
+CURRENT_VERSION=$(curl -s https://registry.hrgn.dk/v2/planning-poker-api/tags/list | jq '.tags | map(select(. | contains ("v"))) |join("\n")' --raw-output | sort -V - | tac | head -n 1)
 NEXTVERSION=$(echo ${CURRENT_VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
 
+echo "curren tag: ${CURRENT_VERSION}"
 echo "next tag: ${NEXTVERSION}"
 
 TAG=$NEXTVERSION docker compose -f docker-compose.yml -f docker-compose.prod.yml build
