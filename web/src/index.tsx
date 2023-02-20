d99e0660df9bd903f99d3fd9231550f4fbbc5082
Frederik Hørgreen <frederik@hoergreen.dk>
2023-02-20 20:44:17 +0100

    main: remove unused web vitals

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import {createBrowserRouter, Link, RouterProvider,} from "react-router-dom";
 import Lobby from "./Pages/Lobby";
 import Home from "./Pages/Home";
@@ -30,8 +29,3 @@ root.render(
     <RouterProvider router={router}/>
     // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
