9f48dfb44ee370677b9d72c3163fa0aaa9a5e930
Frederik Hørgreen <frederik@hoergreen.dk>
2023-02-20 21:13:30 +0100

    main: include index.tsx in lint

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {createBrowserRouter, Link, RouterProvider,} from "react-router-dom";
-import Lobby from "./Pages/Lobby";
-import Home from "./Pages/Home";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom'
+import Lobby from './Pages/Lobby'
+import Home from './Pages/Home'
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home/>,
-    },
-    {
-        path: "lobby/:lobbyId",
-        element: <Lobby/>,
-    },
-    {
-        path: "*",
-        element: <p>404 <Link className={"text-blue-600 dark:text-blue-500 hover:underline"} to={"/"}><br/>Go home</Link></p>
-    }
-]);
+  {
+    path: '/',
+    element: <Home/>
+  },
+  {
+    path: 'lobby/:lobbyId',
+    element: <Lobby/>
+  },
+  {
+    path: '*',
+    element: <p>404 <Link className={'text-blue-600 dark:text-blue-500 hover:underline'} to={'/'}><br/>Go home</Link></p>
+  }
+])
 
 const root = ReactDOM.createRoot(
-    document.getElementById('root') as HTMLElement
-);
+  document.getElementById('root') as HTMLElement
+)
 
 root.render(
     // <React.StrictMode>
     <RouterProvider router={router}/>
     // </React.StrictMode>
-);
+)
