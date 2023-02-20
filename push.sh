43ecf26438f0c792b52df6bb9fd081f4e75c82af
Frederik Hørgreen <frederik@hoergreen.dk>
2023-02-20 19:31:27 +0100

    main: implement deployment by using repo tags

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 set -e
 
-TAG=v0.0.4 docker compose -f docker-compose.yml -f docker-compose.prod.yml build
-TAG=v0.0.4 docker compose -f docker-compose.yml -f docker-compose.prod.yml push
+CURRENT_VERSION=$(curl -s https://registry.hrgn.dk/v2/planning-poker-api-new/tags/list | jq '.tags | sort | reverse | .[0]' --raw-output)
+NEXTVERSION=$(echo ${CURRENT_VERSION} | awk -F. -v OFS=. '{$NF += 1 ; print}')
+
+echo "next tag: ${NEXTVERSION}"
+
+TAG=$NEXTVERSION docker compose -f docker-compose.yml -f docker-compose.prod.yml build
+TAG=$NEXTVERSION docker compose -f docker-compose.yml -f docker-compose.prod.yml push
